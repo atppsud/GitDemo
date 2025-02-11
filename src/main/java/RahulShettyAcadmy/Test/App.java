df6089f8e8f92288b58a5f81d34ef5b6c2098597
sudam <sudam.jadhav17@gmail.com>
2025-02-11 23:17:42 +0530

    commited by IST

--- src/main/java/RahulShettyAcadmy/Test/App.java
+++ src/main/java/RahulShettyAcadmy/Test/App.java
@@ -1,13 +1,22 @@
 package RahulShettyAcadmy.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public void testa()
+	{
+		System.out.println("added  content");
+	}
+	
+	public void testY()
+	{
+		System.out.println("added  content 2");
+	}
 }
